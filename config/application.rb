c2d6bf64d37af600c63127ed76dfb5b671f95cd0
YUMI NARITA <narita.yumi@canon-its.co.jp>
2016-06-18 02:41:47 +0000

    initial setups

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,13 @@ module Microposts
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
